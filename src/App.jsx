b1b3a577d11ca85f5a18c56ffeb8325aa59e4150
Akshita-Mamgain <akshitamamgain@gmail.com>
2024-11-16 00:19:55 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Navigate from './Components/Navigation/Navigate'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navigate/>
+    </div>
+  )
+}
+
+export default App
+
